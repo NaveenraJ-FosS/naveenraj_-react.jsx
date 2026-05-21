b7eb49249be6d6b61754b37bcd1e54c5ca5ccdb7
NaveenraJ-FosS <120163752+NaveenraJ-FosS@users.noreply.github.com>
2026-05-21 23:18:01 +0530

    Add API_Call component for fetching data
    Implement API call using fetch and useEffect hooks.

--- assignment 3/Api_call.jsx
+++ assignment 3/Api_call.jsx
@@ -1,1 +1,24 @@
 
+import React from "react";
+import { useState,useEffect } from "react";
+
+
+
+const API_Call =(url) =>{
+    const [data, setData]=useState(null);
+
+    useEffect(()=> {
+        if(!url)return;
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data); // check structure
+            setData(data);
+        });
+    }, [url]);//runs every time url changes => run when state changes (dependency)
+
+    return data;
+};
+
+export default API_Call
